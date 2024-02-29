9421607fefcdac9c9e2c28859d47571a71a41b45
Altemode <send@geth.gr>
2024-02-29 13:52:45 +0200

    chnages

--- jumpmetric/tests.py
+++ jumpmetric/tests.py
@@ -8,15 +8,7 @@ from jumpmetric.models import Trial
 class TestTrial(TestCase):
     def test_setUp(self) -> None:
         fullnames = [
-            "fullname", "fullnametest",
-            "height", "190",
-            "weight", "89",
-            "age" , "29",
-            "email" , "test@test.test",
-            "occupy" , "studenttest", 
-            "type_of_trial" , "CMJ",
-            "drop_jump_height" , "10",
-            "filename" , "filenametest"
+            "fullname", "190", "89", "29", "test@test.test", "studentTest", "CMJ", "10", "filenameTEst"
         ]
         for item in fullnames:
             Trial.objects.create(fullname=item, height=item, weight=item, age=item, email=item, occupy=item, type_of_trial=item, drop_jump_height=item, filename=item)
